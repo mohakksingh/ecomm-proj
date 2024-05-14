9ab6d57f7fdb29275e6f05a84a19a25f8fc2c568
navyakaushik <navyakaushik678@gmail.com>
2024-05-15 03:45:23 +0530

    frontend(to be resolved)

---
+++ src/App.js
@@ -0,0 +1,27 @@
+//import logo from './logo.svg';
+import './App.css';
+//import './ecomm';
+
+
+import Navigation from './customer/components/Navigation/Navigation.jsx';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation/>
+      <div>
+        home
+      </div>
+    </div>
+  );
+}
+/* styles.css */
+
+//@tailwind base;
+//@tailwind components;
+//@tailwind utilities;
+
+/* Your custom styles here */
+
+
+export default App;
